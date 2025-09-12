272f9b583b7f491ebef9c9e5a183fb7b4f6f4ed9
matveyyy616 <MATVEJNIK05@GMAIL.COM>
2025-09-12 19:35:13 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,14 @@ import express from 'express';
 import TelegramBot from 'node-telegram-bot-api';
 import 'dotenv/config';
 
-// === Настройки ===
+// Настройки
 const token = process.env.BOT_TOKEN;
 const webAppUrl = process.env.WEBAPP_URL;
 
-// === Инициализация бота ===
+// Инициализация бота
 const bot = new TelegramBot(token, { polling: true });
 
-// === Команда /start ===
+// /start
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, 'Выберите тест:', {
     reply_markup: {
@@ -20,7 +20,7 @@ bot.onText(/\/start/, (msg) => {
   });
 });
 
-// === Приём данных из WebApp ===
+// Приём данных из WebApp
 bot.on('message', (msg) => {
   if (msg?.web_app_data?.data) {
     try {
@@ -35,9 +35,8 @@ bot.on('message', (msg) => {
   }
 });
 
-// === Мини‑сервер для Render ===
+// Мини‑сервер для Render
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => res.send('Bot is running'));
 app.listen(port, () => console.log(`Server listening on port ${port}`));
